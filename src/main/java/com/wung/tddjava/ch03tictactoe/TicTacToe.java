c8d3638eeeeb7948d36342afc231ce53077ba050
wungmc <wungcc@gmail.com>
2019-07-17 17:30:03 +0800

    exception

--- src/main/java/com/wung/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/wung/tddjava/ch03tictactoe/TicTacToe.java
@@ -4,7 +4,32 @@
 package com.wung.tddjava.ch03tictactoe;
 
 /**
+ * 井字游戏：玩家分为X和O，在一个 3*3 的棋盘上轮流放子，先在横、竖或者对角线上放满 3 个子的胜出。
+ *
  * @author wung 2019-07-17.
  */
 public class TicTacToe {
+	
+	private Character[][] board = {
+			{'\0', '\0', '\0'},
+			{'\0', '\0', '\0'},
+			{'\0', '\0', '\0'}
+	};
+	
+	public void play(int x, int y) {
+		if (x < 1 || x > 3) {
+			throw new RuntimeException("X is outside board!");
+		}
+		if (y < 1 || y > 3) {
+			throw new RuntimeException("Y is outside board!");
+		}
+		
+		if (board[x - 1][y - 1] != '\0') {
+			throw new RuntimeException("Box is occupied");
+		}
+		else {
+			board[x - 1][y - 1] = 'X';
+		}
+	}
+	
 }
--- src/test/java/com/wung/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/wung/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -3,8 +3,44 @@
  */
 package com.wung.tddjava.ch03tictactoe;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
+ *
  * @author wung 2019-07-17.
  */
 public class TicTacToeSpec {
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	private TicTacToe ticTacToe;
+	
+	@Before
+	public final void before() {
+		ticTacToe = new TicTacToe();
+	}
+	
+	@Test
+	public void whenXOutsideBoardThenThrowRuntimeException() {
+		exception.expect(RuntimeException.class);
+		ticTacToe.play(5, 2);
+	}
+	
+	@Test
+	public void whenYOutsideBoardThenThrowRuntimeException() {
+		exception.expect(RuntimeException.class);
+		ticTacToe.play(2, 5);
+	}
+	
+	@Test
+	public void whenOccupiedThenThrowRuntimeException() {
+		ticTacToe.play(2, 1);
+		exception.expect(RuntimeException.class);
+		ticTacToe.play(2, 1);
+	}
+	
 }
